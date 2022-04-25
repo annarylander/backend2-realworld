41ae9ec83a7d9177470a32fe7b45fbc9f70cc9a8
Anna Rylander <annarylander@MacBook-Air-som-tillhor-Anna.local>
2022-04-25 16:34:26 +0200

    unfavorite

--- controllers/articles.js
+++ controllers/articles.js
@@ -6,8 +6,11 @@ const {
   getArticleBySlugModel,
   updateArticleBySlugModel,
   setFavoriteArticleModel,
+  getArticlesByFavorited,
   removeFavoriteArticleModel,
 } = require("../models/Article");
+
+const { User } = require("../models/User");
 
 const createArticle = async (req, res) => {
   const { title, description, body, tagList } = req.body.article;
@@ -30,6 +33,7 @@ const createArticle = async (req, res) => {
 const getAllArticles = async (req, res) => {
   const author = req.query.author;
   const tag = req.query.tag;
+  const favorited = req.query.favorited;
 
   if (author) {
     try {
@@ -42,6 +46,10 @@ const getAllArticles = async (req, res) => {
     }
   } else if (tag) {
     const articles = await getArticlesByTag(tag);
+    const articlesCount = articles.length;
+    res.json({ articles, articlesCount });
+  } else if (favorited) {
+    const articles = await getArticlesByFavorited(favorited);
     const articlesCount = articles.length;
     res.json({ articles, articlesCount });
   } else {
